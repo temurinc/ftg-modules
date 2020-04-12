18a62881d7e8f795bd87cf32a1cd942931aa29e8
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-04-12 18:17:08 +0300

    Update userpic.py

--- userpic.py
+++ userpic.py
@@ -45,9 +45,9 @@ async def _(event):
     except Exception as e:
         dc_id = "Need a Profile Picture to check **this**"
         location = str(e)
-    caption = """Profile Pic Grabber By @NeoMatrix90
+    caption = """Лови аватарку
 
-Rendi: <a href='tg://user?id={}'>{}</a>
+Она принадлежит ему: <a href='tg://user?id={}'>{}</a>
 """.format(
         user_id,
         user_id,
