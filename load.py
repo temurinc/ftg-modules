3c29474a0806b05b18423daa499e551b1e7e82f6
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-04-17 22:54:37 +0300

    Create load.py

---
+++ load.py
@@ -0,0 +1,77 @@
+""" .geta ник_канала [загрузит все медиа из канала, но максимум 3000 из-за ограничений API.]
+.getc number_of_messsages channel_username  """
+
+ 
+from telethon import events
+import asyncio
+import os
+import subprocess
+import sys
+from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
+
+@borg.on(events.NewMessage(pattern=r"\.getc", outgoing=True))
+async def get_media(event):
+    if event.fwd_from:
+        return
+    dir= "./temp/"
+    try:
+        os.makedirs("./temp/")
+    except:
+    	pass
+    channel_username= event.text
+    command = ['ls','temp','|','wc','-l' ]
+    limit = channel_username[6:9]
+    print(limit)
+    channel_username = channel_username[11: ]
+    print(channel_username)
+    await event.edit("Загружаю медиа из этого канала.")
+    msgs = await borg.get_messages(channel_username, limit=int(limit))
+    with open('log.txt','w') as f:
+    	f.write(str(msgs))
+    for msg in msgs:
+       if msg.media is not None:
+	        await borg.download_media(
+                msg,dir)
+    ps = subprocess.Popen(('ls', 'temp'), stdout=subprocess.PIPE)
+    output = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
+    ps.wait()
+    output = str(output)
+    output = output.replace("b'","")
+    output = output.replace("\n'","")
+    await event.edit("Загружено "+output+" файлов.")
+             
+             
+             
+             
+             
+             
+@borg.on(events.NewMessage(pattern=r"\.geta", outgoing=True))
+async def get_media(event):
+    if event.fwd_from:
+        return
+    dir= "./temp/"
+    try:
+        os.makedirs("./temp/")
+    except:
+    	pass
+    channel_username= event.text
+    command = ['ls','temp','|','wc','-l' ]
+    channel_username = channel_username[7:]
+ 
+   
+    print(channel_username)
+    await event.edit("Загружаю ВСЁ медиа из канала.")
+    msgs = await borg.get_messages(channel_username,limit=3000)
+    with open('log.txt','w') as f:
+    	f.write(str(msgs))
+    for msg in msgs:
+       if msg.media is not None:
+	        await borg.download_media(
+                msg,dir)          
+    ps = subprocess.Popen(('ls', 'temp'), stdout=subprocess.PIPE)
+    output = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
+    ps.wait()
+    output = str(output)
+    output = output.replace("b'","")
+    output = output.replace("\n'","")
+    await event.edit("Загружено "+output+" файлов.")
