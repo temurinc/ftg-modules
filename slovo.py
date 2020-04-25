c1d0579ffa6e770ccbd13468891ab17cbbb48bba
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-04-25 12:30:03 +0300

    Update slovo.py

--- slovo.py
+++ slovo.py
@@ -9,5 +9,5 @@ from uniborg import util
 async def payf(event):
     paytext=event.pattern_match.group(1)
     b = (' ')
-    pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*18, paytext*18, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, b*7 + paytext * 4, )
+    pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*18, paytext*18, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, b*11 + paytext * 4, )
     await event.edit(pay)
