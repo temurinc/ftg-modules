9c6fbf530877437809e03936624f11dae5053848
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-04-30 11:58:56 +0300

    Create lmall.py

---
+++ lmall.py
@@ -0,0 +1,507 @@
+"""
+
+Давай я найду в Google / YouTube / DuckDuckGo / altnews / Xvideo / Xvideos2/ Pornhub / var / log / Dyno для тебя! 
+
+Syntax:
+
+ .lmg <поисковый запрос>
+
+ .lmy <поисковый запрос>
+ 
+ .lmd <поисковый запрос>
+
+ .lmnews <поиск новостей>
+
+ .lmx <поиск xvideos>
+ 
+ .lmx2 <поиск xvideos2>
+
+ .lmp <поиск pornhub>
+
+ .lmvar <название приложения heroku>
+
+ .lmlog <название приложения heroku>
+
+ .dyno <тип биллинга>
+ 
+
+
+
+"""
+
+
+
+
+
+
+
+from telethon import events
+
+
+
+import os
+
+
+
+import requests
+
+
+
+import json
+
+
+
+from uniborg.util import admin_cmd
+
+
+
+
+
+
+
+
+
+
+
+@borg.on(admin_cmd(pattern="lmg (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=http://google.com/search?q={}".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **ГУУУГЛ** специально для тебя:\n👉 [{}]({})\n`Лови ссылку, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+
+
+
+
+@borg.on(admin_cmd(pattern="lmy (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://www.youtube.com/results?search_query={}".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **Ютуб** специально для тебя:\n👉 [{}]({})\n`Лови ссылку, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+#Ну вот что ты тут забыл?
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+
+
+
+@borg.on(admin_cmd(pattern="lmd (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://duckduckgo.com/?q={}&t=h_&ia=about".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **DuckDuckGo** специально для тебя:\n👉 [{}]({})\n`Лови ссылку, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+
+
+
+@borg.on(admin_cmd(pattern="lmx (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://www.xvideos.com/?k={}".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **xvideo** специально для тебя:\n👉 [{}]({})\n`Не натри мозоли, и не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+
+
+
+@borg.on(admin_cmd(pattern="lmp (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://www.pornhub.com/video/search?search={}".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **pornhub** специально для тебя:\n👉 [{}]({})\n`Мой совет - найди себе девушку/парня, и не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+
+@borg.on(admin_cmd(pattern="lmnews (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://www.rbc.ua/rus/search?search_text={}submit=".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **rbc** специально для тебя:\n👉 [{}]({})\n`Лови ссылку, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+
+
+@borg.on(admin_cmd(pattern="lmvar (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://dashboard.heroku.com/apps/{}/settings".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **var** специально для тебя:\n👉 [{}]({})\n`Лови ссылку, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+
+
+@borg.on(admin_cmd(pattern="lmlog (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://dashboard.heroku.com/apps/{}/logs".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **log** специально для тебя:\n👉 [{}]({})\n`Лови ссылку, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+
+        
+        
+@borg.on(admin_cmd(pattern="lmx2 (.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://www.xvideos2.com/?k={}".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **xvideos2** специально для тебя:\n👉 [{}]({})\n`Хорошего времяпровождения, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
+        
+      
+
+
+@borg.on(admin_cmd(pattern="dyno(.*)"))
+
+
+
+async def _(event):
+
+
+
+    if event.fwd_from:
+
+
+
+        return
+
+
+
+    input_str = event.pattern_match.group(1)
+
+
+
+    sample_url = "https://da.gd/s?url=https://dashboard.heroku.com/account/{}".format(input_str.replace(" ","+"))
+
+
+
+    response_api = requests.get(sample_url).text
+
+
+
+    if response_api:
+
+
+
+        await event.edit("Я проверю **Dyno** специально для тебя:\n👉 [{}]({})\n`Лови ссылку, не забудь сказать спасибо! 😉` ".format(input_str,response_api.rstrip()))
+
+
+
+    else:
+
+
+
+        await event.edit("Хьюстон, у нас проблемы! Попробуй позже.")
