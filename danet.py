05f6e14539a9182566cfdc5f558e65f77baf3130
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-05-25 13:30:46 +0300

    Create danet.py

---
+++ danet.py
@@ -0,0 +1,23 @@
+"""Немного заморочливо принимаем решение)
+Команда: .danet"""
+from telethon import events
+import requests
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("danet"))
+async def _(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+    r = requests.get("https://yesno.wtf/api").json()
+    await borg.send_message(
+        event.chat_id,
+        r["answer"],
+        reply_to=message_id,
+        file=r["image"]
+    )
+    await event.delete()
+    #Зачем ты это читал?
