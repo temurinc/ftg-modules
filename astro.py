8a6c22dcf04aaee919a3882546558b4e8342d86d
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-06-03 19:47:22 +0300

    Create astro.py

---
+++ astro.py
@@ -0,0 +1,35 @@
+"""Получаем Астрономическое фото дня (Astronomy Picture of the Day), может потом добавлю архив
+Написал Айвен, https://github.com/AivenGog/ftg, кто удалит, тот плохой человек, неуважающий труд других
+Синтаксис: .apod  """
+
+import time
+from uniborg.util import admin_cmd
+from telethon import events
+import requests
+
+
+@borg.on(events.NewMessage(pattern=r"\.apod", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    response_api = requests.get("https://api.nasa.gov/planetary/apod?api_key=YDNm230oBE5IQdDenyQCzB5P62Hhc9EAJcLxKHE3").json()
+    await event.edit(
+            """
+**Дата**: {} 
+**Название**: {}
+``Описание``: {}
+
+
+__Ссылка__: {}
+__HD (если есть)__: {}
+Главный космонавт: @aivengog
+            """.format(
+                response_api["date"],
+                response_api["title"],
+                response_api["explanation"],
+                response_api["url"],
+                response_api["hdurl"],
+                      )
+
+                     )
+    #Ты заблудился?) пишет Айвен, не удаляй. Дальше будет больше модулей!
