d96425fc3656cbbea505e3047e577a1ebb7f93e7
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-06-19 13:50:19 +0300

    обновил архиватор/дезархиватор

--- archive.py
+++ archive.py
@@ -3,23 +3,28 @@
 #.terminal pip3 install pySmartDL
 #.restart
 #Также вы можете поменять папку временного сохранения в конфигурации.
-#Не пугайтесь, мне просто удобней писать на английском.
 #
 #.zip
 #.unzip
+#.unrar
+# Также спасибо @By_Azade
 
 
 # requires: pySmartDL
 # requires: hachoir
 # requires: datetime
 # requires: zipfile
-
+# requires: patoolib
+# requires: subprocess
+# requires: sample_config
 
 
 import asyncio
 import os
 import time
 import zipfile
+import patoolib
+import subprocess
 from telethon import events
 from pySmartDL import SmartDL
 from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
@@ -28,6 +33,8 @@ from datetime import datetime
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from zipfile import ZipFile
+from datetime import datetime
+from sample_config import Config
 
 
 
@@ -115,7 +122,7 @@ async def _(event):
                 except Exception as e:
                     await borg.send_message(
                         event.chat_id,
-                        "{} caused `{}`".format(caption_rts, str(e)),
+                        "{} вызвано `{}`".format(caption_rts, str(e)),
                         reply_to=event.message.id
                     )
                     # с некоторыми медиа проблемка
@@ -182,3 +189,104 @@ def zipdir(path, ziph):
         for file in files:
             ziph.write(os.path.join(root, file))
             os.remove(os.path.join(root, file))
+            
+            
+@borg.on(admin_cmd(pattern="unrar"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mone = await event.edit("В процессе...")
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        reply_message = await event.get_reply_message()
+        try:
+            c_time = time.time()
+            downloaded_file_name = await borg.download_media(
+                reply_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, mone, c_time, "пытаюсь загрузить")
+                )
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await mone.edit(str(e))
+        else:
+            end = datetime.now()
+            ms = (end - start).seconds
+            await mone.edit("Вскрыл RAR `{}` за {} секунд.".format(downloaded_file_name, ms))
+
+        patoolib.extract_archive(downloaded_file_name, outdir=extracted)
+        filename = sorted(get_lst_of_files(extracted, []))
+        #filename = filename + "/"
+        await event.edit("Сейчас вскроем")
+        # r=root, d=directories, f = files
+        for single_file in filename:
+            if os.path.exists(single_file):
+                # https://stackoverflow.com/a/678242/4723940 спасиб большое
+                caption_rts = os.path.basename(single_file)
+                force_document = True
+                supports_streaming = False
+                document_attributes = []
+                if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
+                    metadata = extractMetadata(createParser(single_file))
+                    duration = 0
+                    width = 0
+                    height = 0
+                    if metadata.has("duration"):
+                        duration = metadata.get('duration').seconds
+                    if os.path.exists(thumb_image_path):
+                        metadata = extractMetadata(createParser(thumb_image_path))
+                        if metadata.has("width"):
+                            width = metadata.get("width")
+                        if metadata.has("height"):
+                            height = metadata.get("height")
+                    document_attributes = [
+                        DocumentAttributeVideo(
+                            duration=duration,
+                            w=width,
+                            h=height,
+                            round_message=False,
+                            supports_streaming=True
+                        )
+                    ]
+                try:
+                    await borg.send_file(
+                        event.chat_id,
+                        single_file,
+                        caption=f"Вскрыл RAR `{caption_rts}`",
+                        force_document=force_document,
+                        supports_streaming=supports_streaming,
+                        allow_cache=False,
+                        reply_to=event.message.id,
+                        attributes=document_attributes,
+                        # progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                        #     progress(d, t, event, c_time, "trying to upload")
+                        # )             не смотрите сюда!
+                    )
+                except Exception as e:
+                    await borg.send_message(
+                        event.chat_id,
+                        "{} вызвано `{}`".format(caption_rts, str(e)),
+                        reply_to=event.message.id
+                    )
+                    # это у нас были проблемы с отображением разных медиа-файлов
+                    continue
+                os.remove(single_file)
+        os.remove(downloaded_file_name)
+
+
+
+
+
+
+
+def get_lst_of_files(input_directory, output_lst):
+    filesinfolder = os.listdir(input_directory)
+    for file_name in filesinfolder:
+        current_file_name = os.path.join(input_directory, file_name)
+        if os.path.isdir(current_file_name):
+            return get_lst_of_files(current_file_name, output_lst)
+        output_lst.append(current_file_name)
+    return output_lst
