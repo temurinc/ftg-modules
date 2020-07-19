29384727bb2323dcc2ea6d300fe4cedf300fcaae
AivenGog <58708238+AivenGog@users.noreply.github.com>
2020-07-19 22:50:20 +0300

    Hmmm

--- antitag.py
+++ antitag.py
@@ -1,6 +1,7 @@
+#made by github.com/AivenGog
 from .. import loader, utils
 
-import logging
+import logging, time
 
 
 from telethon import types
@@ -8,13 +9,13 @@ from telethon import types
 logger = logging.getLogger(__name__)
 
 @loader.tds
-class AFKMod(loader.Module):
+class retMod(loader.Module):
     """Посылает сообщение при вашем теге"""
     strings = {"name": "RFD",
                "gone": "Режим анти-тэга включен",
                "back": "Режим анти-тэга выключен",
-               "afk": "<b>НЕ тэгай меня.</b>",
-               "afk_reason": "{}"}
+               "ret": "<b>НЕ тэгай меня.</b>",
+               "ret_reason": "{}"}
 
     async def client_ready(self, client, db):
         self._db = db
@@ -23,30 +24,29 @@ class AFKMod(loader.Module):
     async def rfdcmd(self, message):
         """.rfd [текст]"""
         if utils.get_args_raw(message):
-            self._db.set(__name__, "afk", utils.get_args_raw(message))
+            self._db.set(__name__, "ret", utils.get_args_raw(message))
         else:
-            self._db.set(__name__, "afk", True)
+            self._db.set(__name__, "ret", self.strings("ret"))
         self._db.set(__name__, "gone", time.time())
-        await self.allmodules.log("afk", data=utils.get_args_raw(message) or None)
-        await utils.answer(message, self.strings("gone", message))
+        await self.allmodules.log("ret", data=utils.get_args_raw(message) or None)
+        await message.edit(self.strings("gone", message))
 
     async def unrfdcmd(self, message):
         """Перестаёт писать"""
-        self._db.set(__name__, "afk", False)
+        self._db.set(__name__, "ret", False)
         self._db.set(__name__, "gone", None)
-        await self.allmodules.log("unafk")
-        await utils.answer(message, self.strings("back", message))
+        await self.allmodules.log("unret")
+        await message.edit(self.strings("back", message))
 
     async def watcher(self, message):
         if not isinstance(message, types.Message):
             return
         if message.mentioned or getattr(message.to_id, "user_id", None) == self._me.id:
-            if self.get_afk() != False:
-                afk_state = self.get_afk()
-                ret = self.strings("afk_reason", message).format(afk_state)
-                await utils.answer(message, ret)
-            else:
-                return
+            if self.get_ret() != False:
+                ret_state = self.get_ret()
+                ret = self.strings("ret_reason", message).format(ret_state)
+                await message.reply(ret)
 
-    def get_afk(self):
-        return self._db.get(__name__, "afk", False)
+
+    def get_ret(self):
+        return self._db.get(__name__, "ret", False)
